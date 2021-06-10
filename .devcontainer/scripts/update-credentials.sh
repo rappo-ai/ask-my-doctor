bb15b5f7490faf0e3c2de13cd5fbfc841ed1c9f4
Renzil DSouza <renzil@gmail.com>
2021-06-10 20:50:19 +0000

    Using custom Telegram connector

--- .devcontainer/scripts/update-credentials.sh
+++ .devcontainer/scripts/update-credentials.sh
@@ -2,7 +2,7 @@
 
 export $(egrep -v '^#' /workspace/.env | xargs)
 
-echo "telegram:
+echo "connectors.telegram.TelegramInput:
     access_token: \"$TELEGRAM_BOT_TOKEN\"
     verify: \"$TELEGRAM_BOT_USERNAME\"
     webhook_url: \"https://$(curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')/webhooks/telegram/webhook\"
