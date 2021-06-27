70819e8743e0e5d39647eb0fccb8d1c5e5d0723f
Renzil DSouza <renzil@gmail.com>
2021-06-27 04:46:27 +0000

    WIP payment stub

--- dataset/actions/action_payment_callback.py
+++ dataset/actions/action_payment_callback.py
@@ -8,11 +8,9 @@ from rasa_sdk.executor import CollectingDispatcher
 from actions.utils.common import (
     create_meeting_link,
     get_admin_group_id,
-    get_appointment_details,
     print_appointment_details,
     print_patient_details,
     print_payment_details,
-    set_payment_details,
 )
 
 logger = logging.getLogger(__name__)
@@ -30,31 +28,36 @@ class ActionPaymentCallback(Action):
     ) -> List[Dict[Text, Any]]:
 
         metadata = tracker.latest_message.get("metadata", {})
-        is_payment_success = metadata.get("is_payment_success", True)
-        if is_payment_success:
-            amount = metadata.get("amount", 300)
-            transaction_id = metadata.get("transaction_id", 1234567890)
-            date = metadata.get("date", "July 1st, 2021 5:15 PM IST")
-            mode = metadata.get("mode", "Credit Card")
-            set_payment_details(amount, transaction_id, date, mode)
+        payment_info: Dict = metadata.get(
+            "payment_info",
+            {
+                "status": "complete",
+                "amount": 300,
+                "transaction_id": "1234567890",
+                "date": "July 1st, 2021 5:15 PM IST",
+                "mode": "Credit Card",
+            },
+        )
+        if payment_info.get("status") == "complete":
+            order_id = get_order_id_for_payment(payment_info)
             meeting_link: Text = create_meeting_link()
-            appointment_details: Dict = get_appointment_details()
+            update_order(order_id, meeting_link=meeting_link, payment_info=payment_info)
             text = (
                 f"Booking Confirmation\n"
                 + "\n"
                 + "Appoinment Details\n"
                 + "\n"
-                + print_appointment_details()
+                + print_appointment_details(order_id)
                 + "\n"
                 + f"Patient details\n"
                 + "\n"
-                + print_patient_details()
+                + print_patient_details(order_id)
                 + "\n"
                 + f"Payment Details\n"
                 + "\n"
-                + print_payment_details()
+                + print_payment_details(order_id)
                 + "\n"
-                + f"Your appointment has been scheduled with {appointment_details.get('doctor_name', '')}. Please join this meeting link at the date and time of the appointment:\n{meeting_link}\n\nIf you need any help with this booking, please click /help."
+                + f"Your appointment has been scheduled. Please join this meeting link at the date and time of the appointment:\n{meeting_link}\n\nIf you need any help with this booking, please click /help."
             )
 
             json_message = {"text": text}
