6453a677fc1be421a0c16de12edcc760da6a7791
Renzil DSouza <renzil@gmail.com>
2021-07-20 21:44:17 +0000

    Adding support for host url

--- .devcontainer/scripts/update-credentials.sh
+++ .devcontainer/scripts/update-credentials.sh
@@ -2,8 +2,16 @@
 
 export $(egrep -v '^#' /app/.env | xargs)
 
+if [ -z "$HOST_URL" ]; then
 echo "connectors.telegram.TelegramInput:
     access_token: \"$TELEGRAM_BOT_TOKEN\"
     verify: \"$TELEGRAM_BOT_USERNAME\"
     webhook_url: \"https://$(curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')/webhooks/telegram/webhook\"
-    host_url: \"https://$(curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')\"" > /app/dataset/credentials.yml
+    host_url: \"https://$(curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')\"" > /app/dataset/credentials.yml;
+else
+echo "connectors.telegram.TelegramInput:
+    access_token: \"$TELEGRAM_BOT_TOKEN\"
+    verify: \"$TELEGRAM_BOT_USERNAME\"
+    webhook_url: \"$HOST_URL/webhooks/telegram/webhook\"
+    host_url: \"$HOST_URL\"" > /app/dataset/credentials.yml;
+fi
